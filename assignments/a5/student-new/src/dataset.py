ef0568af025aa426c51cf834b5c157bdfcbf67c1
lxcreed <806888122@qq.com>
2023-12-26 00:36:32 +0800

    finish a5

--- assignments/a5/student-new/src/dataset.py
+++ assignments/a5/student-new/src/dataset.py
@@ -21,14 +21,16 @@ the same as that of the pretraining dataset.
 You don't need to implement anything in NameDataset.
 """
 
+
 class NameDataset(Dataset):
     def __init__(self, pretraining_dataset, data):
-        self.MASK_CHAR = u"\u2047" # the doublequestionmark character, for mask
-        self.PAD_CHAR = u"\u25A1" # the empty square character, for pad
-        self.itos = pretraining_dataset.itos 
-        self.stoi = pretraining_dataset.stoi 
+        self.MASK_CHAR = u"\u2047"  # the doublequestionmark character, for mask
+        self.PAD_CHAR = u"\u25A1"  # the empty square character, for pad
+        self.itos = pretraining_dataset.itos
+        self.stoi = pretraining_dataset.stoi
         self.block_size = pretraining_dataset.block_size
-        self.data = list(data.encode('utf-8').decode('ascii', errors='ignore').split('\n'))
+        self.data = list(data.encode('utf-8').decode('ascii',
+                         errors='ignore').split('\n'))
 
     def __len__(self):
         # returns the length of the dataset
@@ -39,7 +41,7 @@ class NameDataset(Dataset):
         x = inp + self.MASK_CHAR + oup + self.MASK_CHAR
         x = x + self.PAD_CHAR*(self.block_size - len(x))
         y = self.PAD_CHAR*(len(inp)-1) + x[len(inp):]
-        
+
         x = x[:-1]
         x = torch.tensor([self.stoi[c] for c in x], dtype=torch.long)
         y = torch.tensor([self.stoi[c] for c in y], dtype=torch.long)
@@ -141,19 +143,21 @@ Here are some examples of input-output pairs (x, y):
 
 
 """
+
+
 class CharCorruptionDataset(Dataset):
     def __init__(self, data, block_size):
-        self.MASK_CHAR = u"\u2047" # the doublequestionmark character, for mask
-        self.PAD_CHAR = u"\u25A1" # the empty square character, for pad
+        self.MASK_CHAR = u"\u2047"  # the doublequestionmark character, for mask
+        self.PAD_CHAR = u"\u25A1"  # the empty square character, for pad
 
         chars = list(sorted(list(set(data))))
-        assert self.MASK_CHAR not in chars 
+        assert self.MASK_CHAR not in chars
         assert self.PAD_CHAR not in chars
         chars.insert(0, self.MASK_CHAR)
         chars.insert(0, self.PAD_CHAR)
 
-        self.stoi = { ch:i for i,ch in enumerate(chars) }
-        self.itos = { i:ch for i,ch in enumerate(chars) }
+        self.stoi = {ch: i for i, ch in enumerate(chars)}
+        self.itos = {i: ch for i, ch in enumerate(chars)}
 
         data_size, vocab_size = len(data), len(chars)
         print('data has %d characters, %d unique.' % (data_size, vocab_size))
@@ -168,7 +172,32 @@ class CharCorruptionDataset(Dataset):
 
     def __getitem__(self, idx):
         # TODO [part e]: see spec above
-        raise NotImplementedError
+        document = self.data[idx]
+        length = random.randint(4, int(self.block_size*7/8))
+        if length >= len(document):
+            truncated_document = document
+        else:
+            start_idx = random.randint(0, len(document) - length)
+            truncated_document = document[start_idx:start_idx + length]
+
+        average_masked_length = len(truncated_document) // 4
+        masked_content_length = random.randint(
+            average_masked_length // 2, average_masked_length * 2)
+        start_idx = random.randint(0, length - masked_content_length)
+        prefix = truncated_document[:start_idx]
+        masked_content = truncated_document[start_idx: start_idx +
+                                            masked_content_length]
+        suffix = truncated_document[start_idx + masked_content_length:]
+
+        # 3.
+        rearranged_document = prefix + self.MASK_CHAR + \
+            suffix + self.MASK_CHAR + masked_content
+        rearranged_document += (self.block_size -
+                                len(rearranged_document)) * self.PAD_CHAR
+
+        ipt, opt = rearranged_document[:-1], rearranged_document[1:]
+        return torch.LongTensor([self.stoi[c] for c in ipt]), torch.LongTensor([self.stoi[c] for c in opt])
+
 
 """
 Code under here is strictly for your debugging purposes; feel free to modify
@@ -177,28 +206,29 @@ as desired.
 if __name__ == '__main__':
     argp = argparse.ArgumentParser()
     argp.add_argument('dataset_type', help="Type of dataset to sample from."
-            "Options: namedata, charcorruption.",
-            choices=["namedata", "charcorruption"])
+                      "Options: namedata, charcorruption.",
+                      choices=["namedata", "charcorruption"])
     args = argp.parse_args()
 
     if args.dataset_type == 'namedata':
         # Even if it hasn't been implemented, we use it to define the vocab
-        corruption_dataset = CharCorruptionDataset(open('wiki.txt').read(), 128) 
+        corruption_dataset = CharCorruptionDataset(
+            open('wiki.txt').read(), 128)
         # Make the name dataset
         name_dataset = NameDataset(corruption_dataset,
-            open('birth_places_train.tsv').read())
+                                   open('birth_places_train.tsv').read())
         for _, example in zip(range(4), name_dataset):
             x, y = example
             print('x:', ''.join([name_dataset.itos[int(c)] for c in x]))
             print('y:', ''.join([name_dataset.itos[int(c)] for c in y]))
         pass
     elif args.dataset_type == 'charcorruption':
-        corruption_dataset = CharCorruptionDataset(open('wiki.txt').read(), 128) 
+        corruption_dataset = CharCorruptionDataset(
+            open('wiki.txt').read(), 128)
         for _, example in zip(range(4), corruption_dataset):
             x, y = example
             print('x:', ''.join([corruption_dataset.itos[int(c)] for c in x]))
             print('y:', ''.join([corruption_dataset.itos[int(c)] for c in y]))
     else:
         raise ValueError("Unknown dataset type in command line args: {}"
-                .format(args.dataset_type))
-
+                         .format(args.dataset_type))
--- assignments/a5/student-new/src/london_baseline.py
+++ assignments/a5/student-new/src/london_baseline.py
@@ -2,3 +2,11 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+
+from utils import evaluate_places
+
+DEV_SET = "birth_dev.tsv"
+
+length = len(list(open(DEV_SET)))
+total, correct =evaluate_places(DEV_SET, ['London'] * length)
+print(f"Correct: {correct} out of {total}: {correct / total * 100}%")
